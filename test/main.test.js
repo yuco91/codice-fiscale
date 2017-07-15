954ad0b0326035b0c7ad96e5bfda966a37023d43
Nicola Bonavita <topperblues@yahoo.it>
2017-07-15 09:31:41 +0200

    English translation

--- test/main.test.js
+++ test/main.test.js
@@ -2,5 +2,5 @@ const cf = require('../main');
 
 test('calcola cf', () => {
   expect(cf("mario", "rossi", "M", new Date('1980-1-1'), "udine", "ud")).toBe("RSSMRA80A01L483J");
-  expect(cf("Nicola", "Bonavita", "M", new Date('1984-8-12'), "campobasso")).toBe("BNVNCL84M12B519F");
+  expect(cf("stefania", "bianchi", "F", new Date('2017-09-12'), "SAMATZAI")).toBe("BNCSFN17P52H739A");
 });
